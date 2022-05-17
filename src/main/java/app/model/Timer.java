435ae082ce45b03a8b647ba7ada723768979d48f
Vovo4ka1985 <hammett2010@mail.ru>
2022-05-17 09:56:40 +0400

    new

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,10 @@
 package app.model;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+@Component
+@Primary
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
